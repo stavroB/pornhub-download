0b014f3c418d0353d8247c34e2ccfe4aa54b4319
kweku <45958841+Gouwenkang@users.noreply.github.com>
2020-04-15 15:09:39 +0800

    上传文件

---
+++ content-script.js
@@ -0,0 +1,35 @@
+﻿function Func() {
+	return new Promise((resolve, reject) => {
+		var a = document.querySelector("#player >script:nth-child(2)")
+		if (!a) {
+			a = document.querySelector("#player >script:nth-child(1)")
+		}
+		a = a.innerHTML
+		a = `	var playerObjList = {};\n${a}`
+		var c = a.match("flashvars_[0-9]{1,}")[0]
+		eval(a)
+		var d = eval(c)
+		resolve(d)
+	})
+}
+
+Func().then(res => {
+	var videoType = []
+	Object.keys(res).forEach((item) => {
+		if (item.startsWith('quality_')) {
+			var obj = {
+				key: item,
+				val: res[item],
+				video_title:res.video_title
+			}
+			videoType.push(obj)
+		}
+	})
+	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+		if (request.cmd == 'test') 
+			sendResponse(videoType);
+	});
+})
+
+
+
